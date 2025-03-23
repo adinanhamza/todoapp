084db97348d2567ae42d2b98004836b9887aea18
adinanhamza <adinanck4557@gmail.com>
2025-03-23 16:14:02 +0530

    ui

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,15 +1,225 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+void main() => runApp(TodoApp());
 
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    return MaterialApp(
+      title: 'Pro Todo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        fontFamily: 'Roboto',
+        appBarTheme: AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ),
+      ),
+      home: TodoScreen(),
+    );
   }
+}
+
+class TodoScreen extends StatefulWidget {
+  @override
+  _TodoScreenState createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  final List<Todo> _todos = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addTodo(String task) {
+    if (task.isEmpty) return;
+    
+    setState(() {
+      _todos.add(Todo(
+        id: DateTime.now().millisecondsSinceEpoch.toString(),
+        title: task,
+      ));
+    });
+    _controller.clear();
+  }
+
+  void _toggleTodo(Todo todo) {
+    setState(() {
+      todo.completed = !todo.completed;
+    });
+  }
+
+  void _deleteTodo(String id) {
+    setState(() {
+      _todos.removeWhere((todo) => todo.id == id);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pro Tasks',
+            style: TextStyle(fontWeight: FontWeight.w600)),
+        elevation: 1,
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Icon(Icons.add, size: 28),
+        onPressed: () => _showAddTodoDialog(),
+      ),
+      body: AnimatedSwitcher(
+        duration: Duration(milliseconds: 300),
+        child: _todos.isEmpty
+            ? _buildEmptyState()
+            : ListView.builder(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                itemCount: _todos.length,
+                itemBuilder: (context, index) {
+                  final todo = _todos[index];
+                  return _buildTodoItem(todo);
+                },
+              ),
+      ),
+    );
+  }
+
+  Widget _buildTodoItem(Todo todo) {
+    return Dismissible(
+      key: Key(todo.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 24),
+        child: Icon(Icons.delete, color: Colors.white),
+      ),
+      onDismissed: (_) => _deleteTodo(todo.id),
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: CheckboxListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+          title: Text(todo.title,
+              style: TextStyle(
+                fontSize: 16,
+                decoration: todo.completed
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              )),
+          secondary: Icon(
+            todo.completed ? Icons.task_alt : Icons.task_outlined,
+            color: todo.completed ? Colors.indigo : Colors.grey[400],
+          ),
+          checkColor: Colors.white,
+          activeColor: Colors.indigo,
+          value: todo.completed,
+          onChanged: (_) => _toggleTodo(todo),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmptyState() {
+    return Center(
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        padding: EdgeInsets.all(24),
+        margin: EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: Colors.grey[50],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.assignment_outlined,
+                size: 64, color: Colors.grey[400]),
+            SizedBox(height: 16),
+            Text('No Tasks Found',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.w500,
+                )),
+            SizedBox(height: 8),
+            Text('Tap + to add new task',
+                style: TextStyle(color: Colors.grey[500])),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showAddTodoDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('New Task',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  )),
+              SizedBox(height: 16),
+              TextField(
+                controller: _controller,
+                autofocus: true,
+                decoration: InputDecoration(
+                  hintText: 'Enter task description',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    child: Text('Cancel'),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: Text('Add Task'),
+                    onPressed: () {
+                      _addTodo(_controller.text);
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Todo {
+  final String id;
+  final String title;
+  bool completed;
+
+  Todo({
+    required this.id,
+    required this.title,
+    this.completed = false,
+  });
 }
